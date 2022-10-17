285dfc3272a41313d661a60921592093c48ad05c
Robert Moritz <robert@nott-studios.co.uk>
2022-10-17 15:46:31 +0100

    fix: width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,11 @@ const HomePage: NextPage<Props> = ({ content }) => {
         description="A people-focused Senior Full-stack Developer with a passion for new tech, all things serverless, and reducing code. Experienced in architecting and delivering bespoke, end-to-end, scalable software solutions."
       />
       <Container
-        css={{ my: 'xl', maxWidth: 'xs' }}
-        justify="center"
+        css={{ my: theme.space.xl.value }}
         display="flex"
+        justify="center"
+        responsive={false}
+        xs
       >
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </Container>
