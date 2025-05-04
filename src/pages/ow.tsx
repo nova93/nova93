a39025dcd7f6cea70551351e541694d3801b1870
Rob <rob@duel.tech>
2025-05-04 14:08:05 +0100

    meow

---
+++ src/pages/ow.tsx
@@ -0,0 +1,15 @@
+import Nav from "../components/nav"
+import Seo from "../components/seo"
+
+const MeowPage = () => (
+  <>
+    <Seo
+      title="meow - robmoritz.me"
+    />
+    <Nav />
+    <p>meow?</p>
+    <img src="/cat.gif" width="200px" />
+  </>
+)
+
+export default MeowPage
