67897fa7c28270ec56b0e4899c3cf31fb962d2c5
Rob <rob@duel.tech>
2025-05-04 12:43:09 +0100

    next being a little bitch

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Nav = () => (
   <p>
     <Link href="/me">me</Link> | <Link href="/blog">blog</Link><br />
-    <small>i'm looking for a cool(er) job, shout if you know one</small>
+    <small>i&apos;m looking for a cool(er) job, shout if you know one</small>
   </p>
 )
 
