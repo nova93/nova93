991df1fdb76f195f4eb06c9b7ac7b744010ad3c9
Rob <rob@duel.tech>
2025-05-05 12:56:04 +0100

    blog info

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,6 +4,9 @@ const BlogPage = () => (
   <>
     <Seo title="blog - robmoritz.me" />
     <h1>blog</h1>
+    <p>waiting for inspiration, planned pieces are:</p>
+    <p>push for ai-native</p>
+    <p>process vs culture</p>
   </>
 )
 
