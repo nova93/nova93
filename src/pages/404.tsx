de7acada7a96872860fb72ff76d208eb9d569454
Rob <rob@duel.tech>
2025-05-05 13:35:08 +0100

    not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => (
+  <>
+    <p>You&apos;re lost, let the llama guide you out</p>
+    <img height="200px" src='/llama.gif' />
+    <p>...she&apos;s asleep...</p>
+    <p>i guess you need to find your own way out, sorry</p>
+  </>
+)
+
+export default NotFoundPage;
